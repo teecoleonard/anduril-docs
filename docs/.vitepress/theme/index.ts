03c35ff45ccf9999a722d250daf4df2a8e9e18dc
Leonardo <leonardo4q@gmail.com>
2026-02-11 07:30:22 -0300

    build: gerar dist com reajustes inclusos

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,10 @@
+import { h } from 'vue'
+import type { Theme } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+
+export default {
+  extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout)
+  }
+} satisfies Theme
